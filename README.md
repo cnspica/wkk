ada887002ff6c3c9c2b675835ea106fff8bb6119
wdxtub <dacrocodilee@gmail.com>
2015-03-20 09:05:27 +0800

    readme update and nomadic version update

--- README.md
+++
@@ -1,4 +0,0 @@
-# wkk
-wdx's Knowledge Kit
-
-I'll working on this readme and the project for the following 2 months.
--- nomadic-source/setup.py
+++ nomadic-source/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='nomadic',
-    version='0.4.0',
+    version='0.5.0',
     description='a lightweight note management system.',
     url='https://github.com/wdxtub/wkk',
     author='Da Wang',
@@ -20,11 +20,18 @@ setup(
         'pdfminer',
         'gfm',
         'python-daemon',
-        'watchdog'
+        'watchdog',
+        'flask',
     ],
     entry_points='''
         [console_scripts]
         nomadic=nomadic.cli:cli
+        nomadic-t=nomadic.tools:tools
         nomadic-d=nomadic.daemon:daemon
     ''',
+
+    # Note: this also requires flask-socketio and py-gfm.
+    # These should be installed from git:
+    # pip install git+git://github.com/dart-lang/py-gfm
+    # pip install git+git://github.com/ftzeng/Flask-SocketIO
 )
